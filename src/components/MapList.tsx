cffdf13d1ced637d54cf368fb57768353bf26f7f
vishalpwr <vishalvir8p@gmail.com>
2024-02-12 01:00:00 +0530

    [ADD]: Map list component

---
+++ src/components/MapList.tsx
@@ -0,0 +1,30 @@
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native'
+import React, { JSXElementConstructor, ReactElement } from 'react'
+
+type Props = {
+  data: any[];
+  renderItem: (item: any, index: number) => ReactElement;
+  style?: StyleProp<ViewStyle>;
+  fragment?: boolean;
+};
+
+const MapList = ({ data, renderItem, style, fragment }: Props) => {
+  if (data.length === 0 && !renderItem) return null;
+  const MappedView = () => <>
+    {data.map((item, index) => renderItem(item, index))}
+  </>
+  if (fragment) {
+    return <MappedView />
+  }
+  return (
+    <View style={style}>
+      <MappedView />
+    </View>
+  );
+};
+
+export default MapList
+
+MapList.defaultProps = {
+  fragment: false,
+}
